f822a956e166e2096e2099679a5d07261c19b7e2
jurka14 <56232436+jurka14@users.noreply.github.com>
2021-09-03 16:23:15 +0200

    repo upload

---
+++ proj1/c206/c206.c
@@ -0,0 +1,446 @@
+
+/* c206.c **********************************************************}
+{* Téma: Dvousmìrnì vázaný lineární seznam
+**
+**                   Návrh a referenèní implementace: Bohuslav Køena, øíjen 2001
+**                            Pøepracované do jazyka C: Martin Tuèek, øíjen 2004
+**                                            Úpravy: Kamil Jeøábek, záøí 2019
+**
+** Implementujte abstraktní datový typ dvousmìrnì vázaný lineární seznam.
+** Užiteèným obsahem prvku seznamu je hodnota typu int.
+** Seznam bude jako datová abstrakce reprezentován promìnnou
+** typu tDLList (DL znamená Double-Linked a slouží pro odlišení
+** jmen konstant, typù a funkcí od jmen u jednosmìrnì vázaného lineárního
+** seznamu). Definici konstant a typù naleznete v hlavièkovém souboru c206.h.
+**
+** Vaším úkolem je implementovat následující operace, které spolu
+** s výše uvedenou datovou èástí abstrakce tvoøí abstraktní datový typ
+** obousmìrnì vázaný lineární seznam:
+**
+**      DLInitList ...... inicializace seznamu pøed prvním použitím,
+**      DLDisposeList ... zrušení všech prvkù seznamu,
+**      DLInsertFirst ... vložení prvku na zaèátek seznamu,
+**      DLInsertLast .... vložení prvku na konec seznamu,
+**      DLFirst ......... nastavení aktivity na první prvek,
+**      DLLast .......... nastavení aktivity na poslední prvek,
+**      DLCopyFirst ..... vrací hodnotu prvního prvku,
+**      DLCopyLast ...... vrací hodnotu posledního prvku,
+**      DLDeleteFirst ... zruší první prvek seznamu,
+**      DLDeleteLast .... zruší poslední prvek seznamu,
+**      DLPostDelete .... ruší prvek za aktivním prvkem,
+**      DLPreDelete ..... ruší prvek pøed aktivním prvkem,
+**      DLPostInsert .... vloží nový prvek za aktivní prvek seznamu,
+**      DLPreInsert ..... vloží nový prvek pøed aktivní prvek seznamu,
+**      DLCopy .......... vrací hodnotu aktivního prvku,
+**      DLActualize ..... pøepíše obsah aktivního prvku novou hodnotou,
+**      DLSucc .......... posune aktivitu na další prvek seznamu,
+**      DLPred .......... posune aktivitu na pøedchozí prvek seznamu,
+**      DLActive ........ zjišuje aktivitu seznamu.
+**
+** Pøi implementaci jednotlivých funkcí nevolejte žádnou z funkcí
+** implementovaných v rámci tohoto pøíkladu, není-li u funkce
+** explicitnì uvedeno nìco jiného.
+**
+** Nemusíte ošetøovat situaci, kdy místo legálního ukazatele na seznam
+** pøedá nìkdo jako parametr hodnotu NULL.
+**
+** Svou implementaci vhodnì komentujte!
+**
+** Terminologická poznámka: Jazyk C nepoužívá pojem procedura.
+** Proto zde používáme pojem funkce i pro operace, které by byly
+** v algoritmickém jazyce Pascalovského typu implemenovány jako
+** procedury (v jazyce C procedurám odpovídají funkce vracející typ void).
+**/
+
+#include "c206.h"
+
+int solved;
+int errflg;
+
+void DLError() {
+/*
+** Vytiskne upozornìní na to, že došlo k chybì.
+** Tato funkce bude volána z nìkterých dále implementovaných operací.
+**/
+    printf ("*ERROR* The program has performed an illegal operation.\n");
+    errflg = TRUE;             /* globální promìnná -- pøíznak ošetøení chyby */
+    return;
+}
+
+void DLInitList (tDLList *L) {
+/*
+** Provede inicializaci seznamu L pøed jeho prvním použitím (tzn. žádná
+** z následujících funkcí nebude volána nad neinicializovaným seznamem).
+** Tato inicializace se nikdy nebude provádìt nad již inicializovaným
+** seznamem, a proto tuto možnost neošetøujte. Vždy pøedpokládejte,
+** že neinicializované promìnné mají nedefinovanou hodnotu.
+**/
+    L->Act = NULL;
+    L->Last = NULL;
+    L->First = NULL;
+}
+
+void DLDisposeList (tDLList *L) {
+/*
+** Zruší všechny prvky seznamu L a uvede seznam do stavu, v jakém
+** se nacházel po inicializaci. Rušené prvky seznamu budou korektnì
+** uvolnìny voláním operace free.
+**/
+    struct tDLElem *elemPtr;
+
+    while(L->First != NULL)
+    {
+
+        elemPtr = L->First;
+        L->First = L->First->rptr;
+        free(elemPtr);
+    }
+
+    L->Act = NULL;
+    L->Last = NULL;
+    L->First = NULL;
+}
+
+void DLInsertFirst (tDLList *L, int val) {
+/*
+** Vloží nový prvek na zaèátek seznamu L.
+** V pøípadì, že není dostatek pamìti pro nový prvek pøi operaci malloc,
+** volá funkci DLError().
+**/
+
+	struct tDLElem *newElemPtr = malloc(sizeof(struct tDLElem));
+	if(newElemPtr == NULL)
+    {
+        DLError();
+        return;
+    }
+    newElemPtr->data = val;
+    newElemPtr->rptr = L->First;
+    newElemPtr->lptr = NULL;
+    if (L->First != NULL)
+    {
+        L->First->lptr = newElemPtr;
+    }
+    else
+    {
+        L->Last = newElemPtr;
+    }
+
+    L->First = newElemPtr;
+}
+
+void DLInsertLast(tDLList *L, int val) {
+/*
+** Vloží nový prvek na konec seznamu L (symetrická operace k DLInsertFirst).
+** V pøípadì, že není dostatek pamìti pro nový prvek pøi operaci malloc,
+** volá funkci DLError().
+**/
+
+	struct tDLElem *newElemPtr = malloc(sizeof(struct tDLElem));
+	if(newElemPtr == NULL)
+    {
+        DLError();
+        return;
+    }
+    newElemPtr->data = val;
+    newElemPtr->lptr = L->Last;
+    newElemPtr->rptr = NULL;
+    if (L->Last != NULL)
+    {
+        L->Last->rptr = newElemPtr;
+    }
+    else
+    {
+        L->First = newElemPtr;
+    }
+
+    L->Last = newElemPtr;
+}
+
+void DLFirst (tDLList *L) {
+/*
+** Nastaví aktivitu na první prvek seznamu L.
+** Funkci implementujte jako jediný pøíkaz (nepoèítáme-li return),
+** aniž byste testovali, zda je seznam L prázdný.
+**/
+	L->Act = L->First;
+}
+
+void DLLast (tDLList *L) {
+/*
+** Nastaví aktivitu na poslední prvek seznamu L.
+** Funkci implementujte jako jediný pøíkaz (nepoèítáme-li return),
+** aniž byste testovali, zda je seznam L prázdný.
+**/
+	L->Act = L->Last;
+}
+
+void DLCopyFirst (tDLList *L, int *val) {
+/*
+** Prostøednictvím parametru val vrátí hodnotu prvního prvku seznamu L.
+** Pokud je seznam L prázdný, volá funkci DLError().
+**/
+    if(L->First == NULL)
+    {
+        DLError();
+        return;
+    }
+
+    *val = L->First->data;
+}
+
+void DLCopyLast (tDLList *L, int *val) {
+/*
+** Prostøednictvím parametru val vrátí hodnotu posledního prvku seznamu L.
+** Pokud je seznam L prázdný, volá funkci DLError().
+**/
+    if(L->Last == NULL)
+    {
+        DLError();
+        return;
+    }
+
+    *val = L->Last->data;
+}
+
+void DLDeleteFirst (tDLList *L) {
+/*
+** Zruší první prvek seznamu L. Pokud byl první prvek aktivní, aktivita
+** se ztrácí. Pokud byl seznam L prázdný, nic se nedìje.
+**/
+
+    struct tDLElem *elemPtr;
+    if(L->First != NULL)
+    {
+        elemPtr = L->First;
+        if(L->Act == L->First)
+        {
+            L->Act = NULL;
+        }
+        if(L->First == L->Last)
+        {
+            L->First = NULL;
+            L->Last = NULL;
+        }
+        else
+        {
+            L->First = L->First->rptr;
+            L->First->lptr = NULL;
+        }
+        free(elemPtr);
+    }
+}
+
+void DLDeleteLast (tDLList *L) {
+/*
+** Zruší poslední prvek seznamu L. Pokud byl poslední prvek aktivní,
+** aktivita seznamu se ztrácí. Pokud byl seznam L prázdný, nic se nedìje.
+**/
+
+    struct tDLElem *elemPtr;
+    if(L->First != NULL)
+    {
+        elemPtr = L->Last;
+        if(L->Act == L->Last)
+        {
+            L->Act = NULL;
+        }
+        if(L->First == L->Last)
+        {
+            L->First = NULL;
+            L->Last = NULL;
+        }
+        else
+        {
+            L->Last = L->Last->lptr;
+            L->Last->rptr = NULL;
+        }
+        free(elemPtr);
+    }
+}
+
+void DLPostDelete (tDLList *L) {
+/*
+** Zruší prvek seznamu L za aktivním prvkem.
+** Pokud je seznam L neaktivní nebo pokud je aktivní prvek
+** posledním prvkem seznamu, nic se nedìje.
+**/
+
+    if(L->Act != NULL)
+    {
+        if(L->Act->rptr!= NULL)
+        {
+            struct tDLElem *elemPtr;
+            elemPtr = L->Act->rptr;
+            L->Act->rptr = elemPtr->rptr;
+            if(elemPtr == L->Last)
+            {
+                L->Last = L->Act;
+            }
+            else
+            {
+                elemPtr->rptr->lptr = L->Act;
+            }
+            free(elemPtr);
+        }
+
+    }
+}
+
+void DLPreDelete (tDLList *L) {
+/*
+** Zruší prvek pøed aktivním prvkem seznamu L .
+** Pokud je seznam L neaktivní nebo pokud je aktivní prvek
+** prvním prvkem seznamu, nic se nedìje.
+**/
+
+    if(L->Act != NULL)
+    {
+        if(L->Act->lptr != NULL)
+        {
+            struct tDLElem *elemPtr;
+            elemPtr = L->Act->lptr;
+            L->Act->lptr = elemPtr->lptr;
+            if(elemPtr == L->First)
+            {
+                L->First = L->Act;
+            }
+            else
+            {
+                elemPtr->lptr->rptr = L->Act;
+            }
+            free(elemPtr);
+        }
+
+    }
+}
+
+void DLPostInsert (tDLList *L, int val) {
+/*
+** Vloží prvek za aktivní prvek seznamu L.
+** Pokud nebyl seznam L aktivní, nic se nedìje.
+** V pøípadì, že není dostatek pamìti pro nový prvek pøi operaci malloc,
+** volá funkci DLError().
+**/
+
+    if(L->Act != NULL)
+    {
+        struct tDLElem *newElemPtr = malloc(sizeof(struct tDLElem));
+        if(newElemPtr == NULL)
+        {
+            DLError();
+            return;
+        }
+
+        newElemPtr->data = val;
+        newElemPtr->rptr = L->Act->rptr;
+        newElemPtr->lptr = L->Act;
+        L->Act->rptr = newElemPtr;
+
+        if(L->Act == L->Last)
+        {
+            L->Last = newElemPtr;
+        }
+        else
+        {
+            newElemPtr->rptr->lptr = newElemPtr;
+        }
+    }
+}
+
+void DLPreInsert (tDLList *L, int val) {
+/*
+** Vloží prvek pøed aktivní prvek seznamu L.
+** Pokud nebyl seznam L aktivní, nic se nedìje.
+** V pøípadì, že není dostatek pamìti pro nový prvek pøi operaci malloc,
+** volá funkci DLError().
+**/
+
+    if(L->Act != NULL)
+    {
+        struct tDLElem *newElemPtr = malloc(sizeof(struct tDLElem));
+        if(newElemPtr == NULL)
+        {
+            DLError();
+            return;
+        }
+
+        newElemPtr->data = val;
+        newElemPtr->lptr = L->Act->lptr;
+        newElemPtr->rptr = L->Act;
+        L->Act->lptr = newElemPtr;
+
+        if(L->Act == L->First)
+        {
+            L->First = newElemPtr;
+        }
+        else
+        {
+            newElemPtr->lptr->rptr = newElemPtr;
+        }
+    }
+}
+
+void DLCopy (tDLList *L, int *val) {
+/*
+** Prostøednictvím parametru val vrátí hodnotu aktivního prvku seznamu L.
+** Pokud seznam L není aktivní, volá funkci DLError ().
+**/
+
+    if(L->Act == NULL)
+    {
+        DLError();
+        return;
+    }
+
+    *val = L->Act->data;
+}
+
+void DLActualize (tDLList *L, int val) {
+/*
+** Pøepíše obsah aktivního prvku seznamu L.
+** Pokud seznam L není aktivní, nedìlá nic.
+**/
+    if(L->Act != NULL)
+    {
+        L->Act->data = val;
+    }
+}
+
+void DLSucc (tDLList *L) {
+/*
+** Posune aktivitu na následující prvek seznamu L.
+** Není-li seznam aktivní, nedìlá nic.
+** Všimnìte si, že pøi aktivitì na posledním prvku se seznam stane neaktivním.
+**/
+
+    if(L->Act != NULL)
+    {
+        L->Act = L->Act->rptr;
+    }
+}
+
+
+void DLPred (tDLList *L) {
+/*
+** Posune aktivitu na pøedchozí prvek seznamu L.
+** Není-li seznam aktivní, nedìlá nic.
+** Všimnìte si, že pøi aktivitì na prvním prvku se seznam stane neaktivním.
+**/
+
+    if(L->Act != NULL)
+    {
+        L->Act = L->Act->lptr;
+    }
+}
+
+int DLActive (tDLList *L) {
+/*
+** Je-li seznam L aktivní, vrací nenulovou hodnotu, jinak vrací 0.
+** Funkci je vhodné implementovat jedním pøíkazem return.
+**/
+
+    return((int) L->Act);
+}
+
+/* Konec c206.c*/
---
+++ proj2/c401/c401.c
@@ -0,0 +1,298 @@
+/* c401.c: **********************************************************}
+{* Téma: Rekurzivní implementace operací nad BVS
+**                                         Vytvořil: Petr Přikryl, listopad 1994
+**                                         Úpravy: Andrea Němcová, prosinec 1995
+**                                                      Petr Přikryl, duben 1996
+**                                                   Petr Přikryl, listopad 1997
+**                                  Převod do jazyka C: Martin Tuček, říjen 2005
+**                                         Úpravy: Bohuslav Křena, listopad 2009
+**                                                 Karel Masařík, říjen 2013
+**                                                 Radek Hranický 2014-2018
+**
+** Implementujte rekurzivním způsobem operace nad binárním vyhledávacím
+** stromem (BVS; v angličtině BST - Binary Search Tree).
+**
+** Klíčem uzlu stromu je jeden znak (obecně jím může být cokoliv, podle
+** čeho se vyhledává). Užitečným (vyhledávaným) obsahem je zde integer.
+** Uzly s menším klíčem leží vlevo, uzly s větším klíčem leží ve stromu
+** vpravo. Využijte dynamického přidělování paměti.
+** Rekurzivním způsobem implementujte následující funkce:
+**
+**   BSTInit ...... inicializace vyhledávacího stromu
+**   BSTSearch .... vyhledávání hodnoty uzlu zadaného klíčem
+**   BSTInsert .... vkládání nové hodnoty
+**   BSTDelete .... zrušení uzlu se zadaným klíčem
+**   BSTDispose ... zrušení celého stromu
+**
+** ADT BVS je reprezentován kořenovým ukazatelem stromu (typ tBSTNodePtr).
+** Uzel stromu (struktura typu tBSTNode) obsahuje klíč (typu char), podle
+** kterého se ve stromu vyhledává, vlastní obsah uzlu (pro jednoduchost
+** typu int) a ukazatel na levý a pravý podstrom (LPtr a RPtr). Přesnou definici typů
+** naleznete v souboru c401.h.
+**
+** Pozor! Je třeba správně rozlišovat, kdy použít dereferenční operátor *
+** (typicky při modifikaci) a kdy budeme pracovat pouze se samotným ukazatelem
+** (např. při vyhledávání). V tomto příkladu vám napoví prototypy funkcí.
+** Pokud pracujeme s ukazatelem na ukazatel, použijeme dereferenci.
+**/
+
+#include "c401.h"
+int solved;
+
+void BSTInit (tBSTNodePtr *RootPtr) {
+/*   -------
+** Funkce provede počáteční inicializaci stromu před jeho prvním použitím.
+**
+** Ověřit, zda byl již strom předaný přes RootPtr inicializován, nelze,
+** protože před první inicializací má ukazatel nedefinovanou (tedy libovolnou)
+** hodnotu. Programátor využívající ADT BVS tedy musí zajistit, aby inicializace
+** byla volána pouze jednou, a to před vlastní prací s BVS. Provedení
+** inicializace nad neprázdným stromem by totiž mohlo vést ke ztrátě přístupu
+** k dynamicky alokované paměti (tzv. "memory leak").
+**
+** Všimněte si, že se v hlavičce objevuje typ ukazatel na ukazatel.
+** Proto je třeba při přiřazení přes RootPtr použít dereferenční operátor *.
+** Ten bude použit i ve funkcích BSTDelete, BSTInsert a BSTDispose.
+**/
+
+	//(*RootPtr) = malloc(sizeof(struct tBSTNode));
+	//(*RootPtr)->LPtr = NULL;
+	*RootPtr = NULL;
+
+}
+
+int BSTSearch (tBSTNodePtr RootPtr, char K, int *Content)	{
+/*  ---------
+** Funkce vyhledá uzel v BVS s klíčem K.
+**
+** Pokud je takový nalezen, vrací funkce hodnotu TRUE a v proměnné Content se
+** vrací obsah příslušného uzlu.´Pokud příslušný uzel není nalezen, vrací funkce
+** hodnotu FALSE a obsah proměnné Content není definován (nic do ní proto
+** nepřiřazujte).
+**
+** Při vyhledávání v binárním stromu bychom typicky použili cyklus ukončený
+** testem dosažení listu nebo nalezení uzlu s klíčem K. V tomto případě ale
+** problém řešte rekurzivním volání této funkce, přičemž nedeklarujte žádnou
+** pomocnou funkci.
+**/
+
+    if(RootPtr == NULL)
+    {
+        return 0;
+    }
+
+    if(RootPtr->Key == K)
+    {
+        *Content = RootPtr->BSTNodeCont;
+        return 1;
+    }
+
+    if(RootPtr->Key > K)
+    {
+        return BSTSearch(RootPtr->LPtr, K, Content);
+    }
+
+    return BSTSearch(RootPtr->RPtr, K, Content);
+
+
+
+}
+
+
+void BSTInsert (tBSTNodePtr* RootPtr, char K, int Content)	{
+/*   ---------
+** Vloží do stromu RootPtr hodnotu Content s klíčem K.
+**
+** Pokud již uzel se zadaným klíčem ve stromu existuje, bude obsah uzlu
+** s klíčem K nahrazen novou hodnotou. Pokud bude do stromu vložen nový
+** uzel, bude vložen vždy jako list stromu.
+**
+** Funkci implementujte rekurzivně. Nedeklarujte žádnou pomocnou funkci.
+**
+** Rekurzivní implementace je méně efektivní, protože se při každém
+** rekurzivním zanoření ukládá na zásobník obsah uzlu (zde integer).
+** Nerekurzivní varianta by v tomto případě byla efektivnější jak z hlediska
+** rychlosti, tak z hlediska paměťových nároků. Zde jde ale o školní
+** příklad, na kterém si chceme ukázat eleganci rekurzivního zápisu.
+**/
+
+
+    if((*RootPtr) == NULL) //nenašli jsme, vytvoříme nový uzel
+    {
+        (*RootPtr) = malloc(sizeof(struct tBSTNode));
+        if((*RootPtr) == NULL)
+        {
+            return;
+        }
+
+        (*RootPtr)->Key = K;
+        (*RootPtr)->BSTNodeCont = Content;
+        (*RootPtr)->LPtr = NULL;
+        (*RootPtr)->RPtr = NULL;
+        return;
+    }
+
+    if((*RootPtr)->Key > K)
+    {
+        BSTInsert(&((*RootPtr)->LPtr), K, Content);
+        return ;
+    }
+
+    if((*RootPtr)->Key < K)
+    {
+        BSTInsert(&((*RootPtr)->RPtr), K, Content);
+        return ;
+    }
+
+    //našli jsme
+
+    (*RootPtr)->BSTNodeCont = Content;
+
+}
+
+void ReplaceByRightmost (tBSTNodePtr PtrReplaced, tBSTNodePtr *RootPtr) {
+/*   ------------------
+** Pomocná funkce pro vyhledání, přesun a uvolnění nejpravějšího uzlu.
+**
+** Ukazatel PtrReplaced ukazuje na uzel, do kterého bude přesunuta hodnota
+** nejpravějšího uzlu v podstromu, který je určen ukazatelem RootPtr.
+** Předpokládá se, že hodnota ukazatele RootPtr nebude NULL (zajistěte to
+** testováním před volání této funkce). Tuto funkci implementujte rekurzivně.
+**
+** Tato pomocná funkce bude použita dále. Než ji začnete implementovat,
+** přečtěte si komentář k funkci BSTDelete().
+**/
+
+    tBSTNodePtr RightMost;
+
+    if((*RootPtr)->RPtr == NULL) //kořen nemá pravý podstrom
+    {
+        RightMost = *RootPtr; //kořen je nejpravější
+
+        PtrReplaced->Key = RightMost->Key;
+        PtrReplaced->BSTNodeCont = RightMost->BSTNodeCont;
+
+        if(RightMost->LPtr != NULL) //pokud měl levý podstrom, přemostíme ho
+        {
+            PtrReplaced->LPtr = RightMost->LPtr;
+        }
+        else
+        {
+            PtrReplaced->LPtr = NULL;
+        }
+
+        free(RightMost);
+        return;
+    }
+
+    if((*RootPtr)->RPtr->RPtr == NULL) //ukončení rekurze
+    {
+        RightMost = (*RootPtr)->RPtr;
+
+        //přesun hodnot
+        PtrReplaced->Key = RightMost->Key;
+        PtrReplaced->BSTNodeCont = RightMost->BSTNodeCont;
+
+        //zase přemostění, ale tentokrát napravo
+        if(RightMost->LPtr != NULL)
+        {
+            (*RootPtr)->RPtr = RightMost->LPtr;
+        }
+        else
+        {
+            (*RootPtr)->RPtr = NULL;
+        }
+
+        free(RightMost);
+        return;
+    }
+
+    ReplaceByRightmost(PtrReplaced, &((*RootPtr)->RPtr));//rekurze
+
+}
+
+void BSTDelete (tBSTNodePtr *RootPtr, char K) 		{
+/*   ---------
+** Zruší uzel stromu, který obsahuje klíč K.
+**
+** Pokud uzel se zadaným klíčem neexistuje, nedělá funkce nic.
+** Pokud má rušený uzel jen jeden podstrom, pak jej zdědí otec rušeného uzlu.
+** Pokud má rušený uzel oba podstromy, pak je rušený uzel nahrazen nejpravějším
+** uzlem levého podstromu. Pozor! Nejpravější uzel nemusí být listem.
+**
+** Tuto funkci implementujte rekurzivně s využitím dříve deklarované
+** pomocné funkce ReplaceByRightmost.
+**/
+
+    tBSTNodePtr tmp;
+
+    if((*RootPtr) == NULL)
+    {
+        return;
+    }
+
+
+    if((*RootPtr)->Key > K)
+    {
+        BSTDelete(&((*RootPtr)->LPtr), K);
+        return;
+    }
+
+    if((*RootPtr)->Key < K)
+    {
+        BSTDelete(&((*RootPtr)->RPtr), K);
+        return;
+    }
+    //nalezen uzel s klíčem
+
+    if((*RootPtr)->RPtr == NULL && (*RootPtr)->LPtr == NULL) //nemá podstrom
+    {
+
+        free(*RootPtr);
+        *RootPtr = NULL;
+        return;
+    }
+
+    if((*RootPtr)->LPtr == NULL) //má jen pravý podstrom
+    {
+        tmp = (*RootPtr)->RPtr;
+        free(*RootPtr);
+        *RootPtr = tmp;
+        return;
+    }
+
+    if((*RootPtr)->RPtr == NULL) //má jen levý podstrom
+    {
+        tmp = (*RootPtr)->LPtr;
+        free(*RootPtr);
+        *RootPtr = tmp;
+        return;
+    }
+
+    //má oba podstromy
+
+    ReplaceByRightmost((*RootPtr), &((*RootPtr)->LPtr));
+
+}
+
+void BSTDispose (tBSTNodePtr *RootPtr) {
+/*   ----------
+** Zruší celý binární vyhledávací strom a korektně uvolní paměť.
+**
+** Po zrušení se bude BVS nacházet ve stejném stavu, jako se nacházel po
+** inicializaci. Tuto funkci implementujte rekurzivně bez deklarování pomocné
+** funkce.
+**/
+
+    if(*RootPtr != NULL)
+    {
+        BSTDispose(&((*RootPtr)->LPtr));
+        BSTDispose(&((*RootPtr)->RPtr));
+
+        free(*RootPtr);
+        *RootPtr = NULL;
+    }
+}
+
+/* konec c401.c */
+
